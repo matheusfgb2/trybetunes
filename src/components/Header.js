91976aa3e40db20f43d405ace223dab53aac5191
Matheus Boechat <matheusfgb@gmail.com>
2023-04-13 16:02:28 -0300

    req4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -24,6 +25,9 @@ export default class Header extends Component {
     const { loading, userName } = this.state;
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         {loading ? <Loading /> : <p data-testid="header-user-name">{userName}</p> }
       </header>
     );
