5b06b54ab5c0c4bd3ae80804e591e8eb306e6dbf
Matheus Boechat <matheusfgb@gmail.com>
2023-04-13 16:10:21 -0300

    req5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchArtist: '',
+    btnDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+      btnDisabled: value.length < 2,
+    });
+  };
+
   render() {
+    const { searchArtist, btnDisabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="searchArtist"
+            onChange={ this.handleChange }
+            value={ searchArtist }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ btnDisabled }
+          >
+            Pesquisar
+          </button>
+        </div>
       </>
     );
   }
